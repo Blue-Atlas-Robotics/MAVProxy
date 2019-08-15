78e264c6fff32e1edd694a320d15cca2097c683b
Oleksandr Slovak <slovak194@gmail.com>
2019-08-15 15:11:36 +0200

    -synckup

--- synckup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rsync -avz --exclude '.git' --filter=':- .gitignore' ~/MAVProxy nvidia@poliwhirl:~/slovak/
-
-while true; do
-  inotifywait -r -e modify,create,delete ~/MAVProxy
-  rsync -avz --exclude '.git' --filter=':- .gitignore' ~/MAVProxy nvidia@poliwhirl:~/slovak/
-done
-
